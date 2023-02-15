bb2d0f07ccb74ae566049823acc730fb057215d2
Glen Alangilan <glen@radzoft.com>
2023-02-15 09:04:33 +0800

    clean-up

---
+++ apps/back-office/src/app/lib/load-script.ts
@@ -0,0 +1,8 @@
+export const loadScript = (src:string) => new Promise<void>((resolve, reject) => {
+  if (document.querySelector(`script[src="${src}"]`)) return resolve()
+  const script = document.createElement('script')
+  script.src = src
+  script.onload = () => resolve()
+  script.onerror = (err) => reject(err)
+  document.body.appendChild(script)
+})
